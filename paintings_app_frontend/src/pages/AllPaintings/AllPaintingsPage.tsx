1a74a3154ff0ff23446917f30c9b99b6e4ecfea5
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-03-29 15:51:11 +0200

    refactor: extract PaintingsTableComponent

--- paintings_app_frontend/src/pages/AllPaintings/AllPaintingsPage.tsx
+++ paintings_app_frontend/src/pages/AllPaintings/AllPaintingsPage.tsx
@@ -1,31 +1,15 @@
-import {
-  Button,
-  ButtonGroup,
-  Checkbox,
-  Paper,
-  Stack,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-} from "@mui/material";
+import { Button, Stack } from "@mui/material";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { paintingService } from "../../services/PaintingService";
 import DeletePaintingDialog from "./DeletePaintingDialog";
 import ExportPaintingsDialog from "./ExportPaintingsDialog";
+import PaintingsTableComponent from "./PaintingsTableComponent";
 
 const AllPaintingsPage = () => {
   const navigate = useNavigate();
   const [paintings, setPaintings] = useState(paintingService.getAllPaintings());
   const [selectedPaintings, setSelectedPaintings] = useState([] as number[]);
-  if (selectedPaintings.some((id) => !paintings.some((p) => p.id === id))) {
-    setSelectedPaintings(
-      selectedPaintings.filter((id) => paintings.some((p) => p.id === id))
-    );
-  }
   const [deleteIds, setDeleteIds] = useState([] as number[]);
   const [showExportDialog, setShowExportDialog] = useState(false);
 
@@ -60,80 +44,16 @@ const AllPaintingsPage = () => {
         </Stack>
       </Stack>
 
-      <TableContainer component={Paper} style={{ maxHeight: "70vh" }}>
-        <Table>
-          <TableHead
-            sx={{
-              "& .MuiTableCell-root": { fontWeight: "bold", fontSize: "1em" },
-            }}
-          >
-            <TableRow>
-              <TableCell>
-                <Checkbox
-                  checked={selectedPaintings.length === paintings.length}
-                  indeterminate={
-                    selectedPaintings.length > 0 &&
-                    selectedPaintings.length < paintings.length
-                  }
-                  onChange={(e) => {
-                    if (e.target.checked) {
-                      setSelectedPaintings(paintings.map((p) => p.id));
-                    } else {
-                      setSelectedPaintings([]);
-                    }
-                  }}
-                />
-              </TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Description</TableCell>
-              <TableCell>Year</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {paintings.map((p) => (
-              <TableRow key={p.id}>
-                <TableCell>
-                  <Checkbox
-                    checked={selectedPaintings.includes(p.id)}
-                    onChange={(e) => {
-                      if (e.target.checked) {
-                        setSelectedPaintings([...selectedPaintings, p.id]);
-                      } else {
-                        setSelectedPaintings(
-                          selectedPaintings.filter((id) => id !== p.id)
-                        );
-                      }
-                    }}
-                  />
-                </TableCell>
-                <TableCell>{p.name}</TableCell>
-                <TableCell>{p.description}</TableCell>
-                <TableCell>{p.year}</TableCell>
-                <TableCell>
-                  <ButtonGroup variant="contained">
-                    <Button onClick={() => setDeleteIds([p.id])}>Delete</Button>
-                    <Button
-                      onClick={() =>
-                        navigate("/painting/edit/" + p.id.toString())
-                      }
-                    >
-                      Edit
-                    </Button>
-                    <Button
-                      onClick={() =>
-                        navigate("/painting/details/" + p.id.toString())
-                      }
-                    >
-                      View
-                    </Button>
-                  </ButtonGroup>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <PaintingsTableComponent
+        {...{
+          paintings,
+          selectedPaintings,
+          setSelectedPaintings,
+        }}
+        viewPainting={(id) => navigate("/painting/details/" + id.toString())}
+        editPainting={(id) => navigate("/painting/edit/" + id.toString())}
+        deletePaintings={(ids) => setDeleteIds(ids)}
+      />
 
       <DeletePaintingDialog
         {...{ deleteIds, setDeleteIds, paintings, setPaintings }}
---
+++ paintings_app_frontend/src/pages/AllPaintings/PaintingsTableComponent.tsx
@@ -0,0 +1,105 @@
+import {
+  Button,
+  ButtonGroup,
+  Checkbox,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+interface Props {
+  paintings: Painting[];
+  selectedPaintings: number[];
+  setSelectedPaintings: (ids: number[]) => void;
+  viewPainting: (id: number) => void;
+  editPainting: (id: number) => void;
+  deletePaintings: (ids: number[]) => void;
+}
+
+export default function PaintingsTableComponent(props: Props) {
+  const {
+    paintings,
+    selectedPaintings,
+    setSelectedPaintings,
+    viewPainting,
+    editPainting,
+    deletePaintings,
+  } = props;
+
+  if (selectedPaintings.some((id) => !paintings.some((p) => p.id === id))) {
+    setSelectedPaintings(
+      selectedPaintings.filter((id) => paintings.some((p) => p.id === id))
+    );
+  }
+
+  return (
+    <TableContainer component={Paper} style={{ maxHeight: "70vh" }}>
+      <Table>
+        <TableHead
+          sx={{
+            "& .MuiTableCell-root": { fontWeight: "bold", fontSize: "1em" },
+          }}
+        >
+          <TableRow>
+            <TableCell>
+              <Checkbox
+                checked={selectedPaintings.length === paintings.length}
+                indeterminate={
+                  selectedPaintings.length > 0 &&
+                  selectedPaintings.length < paintings.length
+                }
+                onChange={(e) => {
+                  if (e.target.checked) {
+                    setSelectedPaintings(paintings.map((p) => p.id));
+                  } else {
+                    setSelectedPaintings([]);
+                  }
+                }}
+              />
+            </TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell>Description</TableCell>
+            <TableCell>Year</TableCell>
+            <TableCell>Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {paintings.map((p) => (
+            <TableRow key={p.id}>
+              <TableCell>
+                <Checkbox
+                  checked={selectedPaintings.includes(p.id)}
+                  onChange={(e) => {
+                    if (e.target.checked) {
+                      setSelectedPaintings([...selectedPaintings, p.id]);
+                    } else {
+                      setSelectedPaintings(
+                        selectedPaintings.filter((id) => id !== p.id)
+                      );
+                    }
+                  }}
+                />
+              </TableCell>
+              <TableCell>{p.name}</TableCell>
+              <TableCell>{p.description}</TableCell>
+              <TableCell>{p.year}</TableCell>
+              <TableCell>
+                <ButtonGroup variant="contained">
+                  <Button onClick={() => deletePaintings([p.id])}>
+                    Delete
+                  </Button>
+                  <Button onClick={() => editPainting(p.id)}>Edit</Button>
+                  <Button onClick={() => viewPainting(p.id)}>View</Button>
+                </ButtonGroup>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
