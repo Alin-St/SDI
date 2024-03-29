c9e97ef2f3883bedfa9d3d45e0f99ed2d4faffff
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-03-29 14:57:01 +0200

    add export functionality
    Added the ability to export to CSV or JSON

--- paintings_app_frontend/src/pages/AllPaintingsPage.tsx
+++ paintings_app_frontend/src/pages/AllPaintingsPage.tsx
@@ -4,8 +4,11 @@ import {
   Checkbox,
   Dialog,
   DialogActions,
+  DialogContent,
   DialogTitle,
+  MenuItem,
   Paper,
+  Select,
   Stack,
   Table,
   TableBody,
@@ -13,7 +16,9 @@ import {
   TableContainer,
   TableHead,
   TableRow,
+  TextField,
 } from "@mui/material";
+import Papa from "papaparse";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useToast } from "../contexts/ToastContext";
@@ -21,6 +26,7 @@ import { paintingService } from "../services/PaintingService";
 
 const AllPaintingsPage = () => {
   const navigate = useNavigate();
+  const showToast = useToast();
   const [paintings, setPaintings] = useState(paintingService.getAllPaintings());
   const [selectedPaintings, setSelectedPaintings] = useState([] as number[]);
   if (selectedPaintings.some((id) => !paintings.some((p) => p.id === id))) {
@@ -29,7 +35,17 @@ const AllPaintingsPage = () => {
     );
   }
   const [deleteIds, setDeleteIds] = useState([] as number[]);
-  const showToast = useToast();
+  const [showExportDialog, setShowExportDialog] = useState(false);
+  const [exportType, setExportType] = useState("");
+  if (!showExportDialog && exportType !== "") {
+    setExportType("");
+  }
+  const exportText =
+    exportType === "json"
+      ? JSON.stringify(paintings, null, 2)
+      : exportType === "csv"
+      ? Papa.unparse(paintings) + "HELLLLO "
+      : "";
 
   return (
     <>
@@ -43,13 +59,23 @@ const AllPaintingsPage = () => {
         >
           Delete Selected
         </Button>
-        <Button
-          onClick={() => navigate("/painting/add")}
-          variant="contained"
-          sx={{ m: 1 }}
-        >
-          Add Painting
-        </Button>
+
+        <Stack direction="row">
+          <Button
+            variant="outlined"
+            sx={{ m: 1 }}
+            onClick={() => setShowExportDialog(true)}
+          >
+            Export
+          </Button>
+          <Button
+            onClick={() => navigate("/painting/add")}
+            variant="contained"
+            sx={{ m: 1 }}
+          >
+            Add Painting
+          </Button>
+        </Stack>
       </Stack>
 
       <TableContainer component={Paper} style={{ maxHeight: "70vh" }}>
@@ -152,6 +178,51 @@ const AllPaintingsPage = () => {
           </Button>
           <Button variant="outlined" onClick={() => setDeleteIds([])}>
             No
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      <Dialog
+        open={showExportDialog}
+        fullWidth
+        onClose={() => setShowExportDialog(false)}
+      >
+        <DialogTitle>
+          Export all paintings to{" "}
+          <Select
+            value={exportType}
+            sx={{ minWidth: 100 }}
+            size="small"
+            onChange={(e) => setExportType(e.target.value)}
+          >
+            <MenuItem value="json">JSON</MenuItem>
+            <MenuItem value="csv">CSV</MenuItem>
+          </Select>
+        </DialogTitle>
+        <DialogContent>
+          <TextField
+            multiline
+            inputProps={{ readOnly: true }}
+            value={exportText}
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button
+            variant="outlined"
+            disabled={exportText === ""}
+            onClick={() => {
+              navigator.clipboard.writeText(exportText);
+              showToast("Export copied to clipboard");
+            }}
+          >
+            Copy to Clipboard
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => setShowExportDialog(false)}
+          >
+            Close
           </Button>
         </DialogActions>
       </Dialog>
