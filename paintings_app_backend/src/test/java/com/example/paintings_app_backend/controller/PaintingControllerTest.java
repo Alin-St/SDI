65feb00d13ef5aa1c3e0640caa91209968ffb578
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-04-03 14:10:42 +0300

    add tests to backend service and controller

---
+++ paintings_app_backend/src/test/java/com/example/paintings_app_backend/controller/PaintingControllerTest.java
@@ -0,0 +1,64 @@
+package com.example.paintings_app_backend.controller;
+
+import com.example.paintings_app_backend.domain.Painting;
+import com.example.paintings_app_backend.service.PaintingService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class PaintingControllerTest {
+    private final PaintingService paintingService = Mockito.mock(PaintingService.class);
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        PaintingController paintingController = new PaintingController(paintingService);
+        mockMvc = MockMvcBuilders.standaloneSetup(paintingController).build();
+    }
+
+    @Test
+    public void testGetAll() throws Exception {
+        given(paintingService.getAll()).willReturn(Arrays.asList(new Painting(), new Painting()));
+        mockMvc.perform(get("/api/paintings"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testGetById() throws Exception {
+        given(paintingService.getById(1)).willReturn(Optional.of(new Painting()));
+        mockMvc.perform(get("/api/paintings/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAdd() throws Exception {
+        mockMvc.perform(post("/api/paintings")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"painting 1\",\"description\":\"description 1\",\"publicationYear\":1001}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        mockMvc.perform(put("/api/paintings/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"painting 1\",\"description\":\"description 1\",\"publicationYear\":1001}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        mockMvc.perform(delete("/api/paintings/1"))
+                .andExpect(status().isOk());
+    }
+}
---
+++ paintings_app_backend/src/test/java/com/example/paintings_app_backend/service/PaintingServiceTest.java
@@ -0,0 +1,57 @@
+package com.example.paintings_app_backend.service;
+
+import com.example.paintings_app_backend.domain.Painting;
+import com.example.paintings_app_backend.repository.IPaintingRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.Optional;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
+public class PaintingServiceTest {
+    private final IPaintingRepository paintingRepository = Mockito.mock(IPaintingRepository.class);
+    private PaintingService paintingService;
+
+    @BeforeEach
+    public void setup() {
+        paintingService = new PaintingService(paintingRepository);
+    }
+
+    @Test
+    public void testGetAll() {
+        given(paintingRepository.findAll()).willReturn(Arrays.asList(new Painting(), new Painting()));
+        paintingService.getAll();
+        verify(paintingRepository).findAll();
+    }
+
+    @Test
+    public void testGetById() {
+        given(paintingRepository.findById(1)).willReturn(Optional.of(new Painting()));
+        paintingService.getById(1);
+        verify(paintingRepository).findById(1);
+    }
+
+    @Test
+    public void testAdd() {
+        Painting painting = new Painting();
+        paintingService.add(painting);
+        verify(paintingRepository).save(painting);
+    }
+
+    @Test
+    public void testUpdate() {
+        Painting painting = new Painting();
+        paintingService.update(1, painting);
+        verify(paintingRepository).save(painting);
+    }
+
+    @Test
+    public void testDelete() {
+        paintingService.delete(1);
+        verify(paintingRepository).deleteById(1);
+    }
+}
