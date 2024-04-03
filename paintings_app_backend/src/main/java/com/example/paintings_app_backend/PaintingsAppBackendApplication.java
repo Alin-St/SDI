d3ad897df559704ebd87d21013a7b77586f953c0
Alin Stanciu <alinstanciu2011@yahoo.com>
2024-04-03 05:33:55 +0300

    create project

---
+++ paintings_app_backend/src/main/java/com/example/paintings_app_backend/PaintingsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.paintings_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintingsAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaintingsAppBackendApplication.class, args);
+    }
+
+}
